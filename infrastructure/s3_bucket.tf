73bcd56503ecca5afcb6cf7959df09d81f2b32d9
root <root@ip-172-31-6-49.us-east-2.compute.internal>
2026-01-02 19:05:24 +0000

    multi workspace project completed

--- infrastructure/s3_bucket.tf
+++ infrastructure/s3_bucket.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "s3-bucket" {
-  bucket = "${bucket_name}-${var.env}"
-  
+  bucket = "${var.bucket_name}-${var.env}"
+
   tags = {
     Name        = "${var.env}-bucket"
     Environment = var.env
--- infrastructure/veriable.tf
+++ infrastructure/veriable.tf
@@ -1,15 +1,24 @@
-veriable "env" {
+variable "env" {
   description = "This is environment veriable for inftrasture"
   type        = string
 }
 
-veriable "bucket_name" {
+variable "bucket_name" {
   description = "This is bucket name for inftrasture"
   type        = string
 }
 
-veriable "hash-key" {
+variable "hash-key" {
   description = "This is hash-key name for DynamoDB"
   type        = string
 }
 
+variable "instance_type" {
+  description = "This is instance type"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "This is instance count "
+  type        = number
+}
